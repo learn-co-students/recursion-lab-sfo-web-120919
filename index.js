7601f24ffe399d579980d6ce2e07af0edddfca6a
github username <ingrey1@gmail.com>
2020-06-04 11:53:40 -0700

    added recursive maxOf

--- index.js
+++ index.js
@@ -51,6 +51,17 @@ function addUpTo(nums, index) {
     }
 }
 
+ function maxOf(arr) {
+    //finds largest int in arr
+  
+    if (arr.length <= 1) {
+      return arr[0];
+    } else {
+      arr[0] > arr[1] ? arr.splice(1, 1) : arr.splice(0, 1);
+      return maxOf(arr);
+    }
+  };
+
 
 
 
