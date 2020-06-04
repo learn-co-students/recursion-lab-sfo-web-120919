d2375c81a4d19ad858051878357893873eb320e0
github username <ingrey1@gmail.com>
2020-06-04 10:39:58 -0700

    recursive isPalindrome

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ function reverseString(str) {
         return reverseString(str.substring(1)) + str[0]
     }
 }
+
+function isPalindrome(str) {
+    //console.log("str", str.length)
+    // compare first and last character
+    if (str.length === 1) {
+        return str
+    } else {
+        if (str[0] !== str[str.length - 1]) return false
+        isPalindrome(str.substring(1, str.length - 1))
+    }
+
+    return true
+
+}
