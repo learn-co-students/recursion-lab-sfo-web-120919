cd90922046b07d6964c7dc69eac9cfcab72d482a
github username <ingrey1@gmail.com>
2020-06-04 10:23:05 -0700

    added recursive printString

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // Code your solution here!
 
+function printString(str) {
+    
+    console.log(str[0])
+    if (str.length === 1){
+        
+    } 
+   
+    else {
+        printString(str.substring(1))
+    }
+    
+
+}
